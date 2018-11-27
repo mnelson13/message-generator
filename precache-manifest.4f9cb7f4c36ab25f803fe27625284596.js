e9aa14a1c851239822f6c0a3aa93130f748905ca
MNelson <melissa.nelson13@yahoo.com>
2018-11-26 20:08:10 -0600

    Updates

---
+++ precache-manifest.4f9cb7f4c36ab25f803fe27625284596.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "916fbc5242a89830c1e6f3842d4bae7b",
+    "url": "/message-generator/static/media/light_grey_@2X.916fbc52.jpg"
+  },
+  {
+    "revision": "5247e551856716159a37",
+    "url": "/message-generator/static/js/runtime~main.5247e551.js"
+  },
+  {
+    "revision": "063fd47fcd6e73cb7d5e",
+    "url": "/message-generator/static/js/main.063fd47f.chunk.js"
+  },
+  {
+    "revision": "f48a7d27b77e4e5e38e8",
+    "url": "/message-generator/static/js/1.f48a7d27.chunk.js"
+  },
+  {
+    "revision": "063fd47fcd6e73cb7d5e",
+    "url": "/message-generator/static/css/main.3d8d680a.chunk.css"
+  },
+  {
+    "revision": "f48a7d27b77e4e5e38e8",
+    "url": "/message-generator/static/css/1.ab9d0b15.chunk.css"
+  },
+  {
+    "revision": "424704e172c4f0765ff26b13387c2e2d",
+    "url": "/message-generator/index.html"
+  }
+];
